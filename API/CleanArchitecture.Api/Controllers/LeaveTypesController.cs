b8c6801186b69acac04e6bfd7c4de322557e6522
shayzaran <shayzaran@gmail.com>
2022-08-15 13:25:02 +0300

    Added controllers, endpoints and created db by migrations

---
+++ API/CleanArchitecture.Api/Controllers/LeaveTypesController.cs
@@ -0,0 +1,56 @@
+﻿using CleanArchitecture.Application.DTOs.LeaveType;
+using CleanArchitecture.Application.Features.LeaveTypes.Requests.Commands;
+using CleanArchitecture.Application.Features.LeaveTypes.Requests.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CleanArchitecture.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LeaveTypesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public LeaveTypesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        // GET: api/<LeaveTypesController>
+        [HttpGet]
+        public async Task<ActionResult<List<LeaveTypeDto>>> Get()
+        {
+            var leaveTypes = await _mediator.Send(new GetLeaveTypeListRequest());
+            return Ok(leaveTypes);
+        }
+
+        // GET api/<LeaveTypesController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<LeaveTypeDto>> Get(int id)
+        {
+            var leaveType = await _mediator.Send(new GetLeaveTypeDetailRequest { Id = id });
+            return Ok(leaveType);
+        }
+
+        // POST api/<LeaveTypesController>
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] CreateLeaveTypeDto leaveTypeDto)
+        {
+            var command = new CreateLeaveTypeCommand { LeaveTypeDto = leaveTypeDto };   
+            var response = await _mediator.Send(command);
+            return Ok(response);
+        }
+
+        // PUT api/<LeaveTypesController>
+        [HttpPut]
+        public async Task<ActionResult> Put([FromBody] LeaveTypeDto leaveTypeDto)
+        {
+            var command = new UpdateLeaveTypeCommand { LeaveTypeDto = leaveTypeDto };   
+            await _mediator.Send(command);
+            return NoContent();
+        }
+    }
+}
--- Core/CleanArchitecture.Domain/Common/BaseDomainEntity.cs
+++ Core/CleanArchitecture.Domain/Common/BaseDomainEntity.cs
@@ -10,8 +10,8 @@ namespace CleanArchitecture.Core.Domain.Common
     {
         public int Id { get; set; }
         public DateTime CreatedDate { get; set; }
-        public string CreatedBy { get; set; }
+        public string? CreatedBy { get; set; }
         public DateTime LastModifiedDate { get; set; }
-        public string LastModifiedBy { get; set; }
+        public string? LastModifiedBy { get; set; }
     }
 }
